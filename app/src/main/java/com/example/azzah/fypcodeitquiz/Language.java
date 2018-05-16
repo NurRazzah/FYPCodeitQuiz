97cc37feb5d35b9d0d1a095428354fe4678183be
lutfiah <16023022@myrp.edu.sg>
2018-05-16 16:47:30 +0800

    Cat

---
+++ app/src/main/java/com/example/azzah/fypcodeitquiz/Language.java
@@ -0,0 +1,12 @@
+package com.example.azzah.fypcodeitquiz;
+
+public class Language {
+    private String name;
+
+    public Language(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/azzah/fypcodeitquiz/LanguageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.azzah.fypcodeitquiz;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class LanguageAdapter extends ArrayAdapter {
+    private ArrayList<Language> lang;
+    private Context context;
+    private Button btnLang, btnLang2;
+
+    public LanguageAdapter(Context context, int resource, ArrayList<Language> objects) {
+        super(context, resource);
+        lang = objects;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowVIew = inflater.inflate(R.layout.content_main_row, parent, false);
+
+        btnLang = (Button)rowVIew.findViewById(R.id.cat);
+        btnLang2 = (Button)rowVIew.findViewById(R.id.cat2);
+
+        Language currentLang = lang.get(position);
+        for (int i = 0; i < lang.size(); i++){
+            if(i%2 == 0) {
+                btnLang.setText(currentLang.getName());
+            } else {
+                btnLang2.setText(currentLang.getName());
+            }
+        }
+        //bb
+        return rowVIew;
+    }
+}
